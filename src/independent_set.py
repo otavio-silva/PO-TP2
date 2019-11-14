9bc8a413bfaecaa34cb99663812092892585ba45
Otávio Augusto Silva <otavio.silva@dcc.ufmg.br>
2019-11-14 16:26:26 -0300

    Minor changes

--- src/independent_set.py
+++ src/independent_set.py
@@ -6,11 +6,10 @@ n2 = 2000
 g = networkx.complete_bipartite_graph(n1, n2)
 
 model = mip.Model("Independent Set")
-x = [model.add_var(var_type=mip.BINARY) for _ in range(n1 + n2)]
-model.objective = mip.maximize(mip.xsum(x[i] for i in range(n1 + n2)))
+x = [model.add_var(var_type=mip.BINARY) for _ in range(len(g.nodes))]
+model.objective = mip.maximize(mip.xsum(x[i] for i in range(len(g.nodes))))
 for (i, j) in g.edges:
 	model += mip.xsum([x[i], x[j]]) <= 1
-#model += mip.xsum(x[i] + x[j] for (i, j) in g.edges) <= 1
 model.optimize()
-selected = [i for i in range(n1 + n2) if x[i].x >= 0.99]
+selected = [i for i in range(len(g.nodes)) if x[i].x >= 0.99]
 print(selected)
