4deff204a49e5a633abf84e32e11267a58538063
Otávio Augusto Silva <otavio.silva@dcc.ufmg.br>
2019-11-14 16:28:08 -0300

    Added visualisations

--- src/independent_set.py
+++ src/independent_set.py
@@ -1,9 +1,12 @@
 import mip
 import networkx
+from matplotlib import pyplot
 
 n1 = 1000
 n2 = 2000
 g = networkx.complete_bipartite_graph(n1, n2)
+networkx.drawing.draw_networkx(g)
+pyplot.show()
 
 model = mip.Model("Independent Set")
 x = [model.add_var(var_type=mip.BINARY) for _ in range(len(g.nodes))]
@@ -13,3 +16,6 @@ for (i, j) in g.edges:
 model.optimize()
 selected = [i for i in range(len(g.nodes)) if x[i].x >= 0.99]
 print(selected)
+g1 = g.subgraph(selected)
+networkx.drawing.draw_networkx(g1)
+pyplot.show()
